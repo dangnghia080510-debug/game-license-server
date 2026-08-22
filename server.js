0cbd7070b8016855bd152607b6b8773c9a5f1aea
dangnghia080510-debug <dangnghia080510@gmail.com>
2026-08-22 12:01:17 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sqlite3 = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const crypto = require('crypto');
 const cors = require('cors');
 
